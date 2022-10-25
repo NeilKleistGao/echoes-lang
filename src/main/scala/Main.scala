1b07f5f26df34c8227e40701945c0e11edd25781
NeilKleistGao <neilkleist@gmail.com>
2022-10-25 21:18:10 +0800

    Update License

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,10 @@
+/**
+  * Apache License
+  * Version 2.0, January 2004
+  * http://www.apache.org/licenses/
+  */
+
 object Main {
   def main(args: Array[String]): Unit = {
-    System.out.println("hello world.")
   }
 }
